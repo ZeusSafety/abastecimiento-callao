acfb2e41c2d28a2f820549b23e42dd90bdbb87fb
CibertecEdgar <gameplaysytroleos@gmail.com>
2026-04-06 11:47:38 -0500

    cambios aceptados

--- app/callao/historial-carga/page.tsx
+++ app/callao/historial-carga/page.tsx
@@ -972,7 +972,7 @@ export default function HistorialCargaPage() {
                                     <button
                                         type="button"
                                         onClick={() => setMostrarPassword(!mostrarPassword)}
-                                        className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 transition-colors"
+                                        className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 transition-colors"
                                     >
                                         {mostrarPassword ? (
                                             <EyeOff className="w-4 h-4" />
