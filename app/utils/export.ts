6d1536889648d930a328bbfd65560af1bd72a201
ZeusSafety <zeusadmi24@gmail.com>
2026-09-10 13:28:01 -0500

    Despliegue

--- app/utils/export.ts
+++ app/utils/export.ts
@@ -1,7 +1,7 @@
 import * as XLSX from 'xlsx';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
-
+ 
 // Función para exportar a Excel
 export function exportToExcel(
   data: any[],
